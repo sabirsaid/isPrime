258cb226f8f4db5417edff38224a890f15d79905
sabir <anas.el-alami@pyxicom.com>
2018-08-15 12:25:59 +0100

    - fixer le cas $p = 1 - function get Primes beetwen two number

--- index.php
+++ index.php
@@ -6,9 +6,33 @@ function is_prime($p) {
 	$r2 = $p%3;
 	$r3 = $p%5;
 
-	if ( (($r1 >= 1) && ($r2 >= 1) && ($r3 >= 1)) ||  in_array($p, [2,3,5]) ) {
+	if ( ($p > 1) &&  (($r1 >= 1) && ($r2 >= 1) && ($r3 >= 1)) ||  in_array($p, [2,3,5]) ) {
 	 	$is_prime = true;
 	}
 
 	return $is_prime;
+}
+
+function getPrimes($n1, $n2) {
+    $negatif = false;
+    $nmbPrimes = [];
+    if($n2<0){
+        $negatif = true;
+        $n2 = -$n2;
+    }
+    if($n1<0){
+        $negatif = true;
+        $n1 = -$n1;
+    }
+	for($i=$n1;$i<=$n2;$i++){
+
+        if(is_prime($i)){
+            if($negatif){
+            	$nmrPremisers [] = "-".$i;
+            }
+            $nmbPrimes [] = $i;
+        }
+    }
+
+    return $nmbPrimes;
 }
