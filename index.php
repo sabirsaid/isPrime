4d33dda5c0baade552aa8870f4c18f6270fe2f81
sabir <anas.el-alami@pyxicom.com>
2018-08-15 12:06:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+function is_prime($p) {
+ 	$is_prime = false;
+ 	$r1 = $p%2;
+	$r2 = $p%3;
+	$r3 = $p%5;
+
+	if ( (($r1 >= 1) && ($r2 >= 1) && ($r3 >= 1)) ||  in_array($p, [2,3,5]) ) {
+	 	$is_prime = true;
+	}
+
+	return $is_prime;
+}
