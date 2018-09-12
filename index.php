8ccf6d0eb3797f156e57fdd4a7ddc63c66fd41e3
said sabir <41737127+sabirsaid@users.noreply.github.com>
2018-09-12 15:02:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,18 @@
 <?php
 
 function is_prime($p) {
+	if ($p < 0)
+		$p = -$p;
 	return ($p > 1) &&  (($p%2 >= 1) && ($p%3 >= 1) && ($p%5 >= 1)) ||  in_array($p, [2,3,5]);
 }
 
-function getPrimes($n1, $n2) {
-    $negatif = false;
-    $nmbPrimes = [];
-    if($n2<0){
-        $negatif = true;
-        $n2 = -$n2;
-    }
-    if($n1<0){
-        $negatif = true;
-        $n1 = -$n1;
-    }
-	for($i=$n1;$i<=$n2;$i++){
 
+function getPrimes($n1, $n2) {
+    $nmbPrimes = [];
+    for($i=$n1;$i<=$n2;$i++){
         if(is_prime($i)){
-            if($negatif){
-            	$nmbPrimes [] = "-".$i;
-            }
             $nmbPrimes [] = $i;
         }
     }
-
     return $nmbPrimes;
 }
